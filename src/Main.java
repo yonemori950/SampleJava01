32a18a06e8bc2ae8e11e1eccb480a1f83a919d44
“yonemori” <yonemori950@gmail.com>
2025-05-19 16:41:40 +0900

    ブランチ作成

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,6 @@
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-
 public class Main {
 
 	public static void main(String[] args) {
-
-		List<Integer> list = Arrays.asList(4, 2, 3, 6, 8);
-		Collections.reverse(list); // リストが[4, 2, 1, 6, 8]になる
-		System.out.print(list);
-
-		//List<Integer> list = Arrays.asList(1, 3, 2);
-		Collections.sort(list, Collections.reverseOrder());
-		System.out.print(list);
 
 	}
 }
